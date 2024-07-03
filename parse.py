68093809c5015f06a17a2da54e0082f4cef65282
pablonara <pablonara@pablonara.com>
2024-07-02 17:11:53 -0700

    fixes, parsing works correctly now

--- parse.py
+++ parse.py
@@ -56,31 +56,70 @@ if a {print("hello")}
 def helloWorld() {print("Hello World!")};
 print("Arch user btw uwu"); for i in range(5) {print(i)}
 """
+# if args.interactive == 'y':
+#     console = code.InteractiveConsole()
+#     buffer = []
+#     while True:
+#         a = input("> ")
+#         processed = a
+#         print("processed: ")
+#         print(processed)
+#         output = io.StringIO()
+#         lines = processed.split('\n')
+#         for line in lines:
+#             buffer.append(line)
+#             code = '\n'.join(buffer)
+#             sys.stdout = output
+#             sys.stderr = output
+#             more = console.push(code)
+#             sys.stdout = sys.__stdout__
+#             sys.stderr = sys.__stderr__
+#             if not more: 
+#                 buffer = []
+#                 print(output.getvalue)
+#             else:
+#                 pass
+#         if not more:
+#             print(output.getvalue())
+
 if args.interactive == 'y':
     console = code.InteractiveConsole()
-    buffer = []
+    
     while True:
-        a = input("> ")
-        processed = a
-        print("processed: ")
-        print(processed)
-        output = io.StringIO()
-        lines = processed.split('\n')
-        for line in lines:
-            buffer.append(line)
-            code = '\n'.join(buffer)
-            sys.stdout = output
-            sys.stderr = output
-            more = console.push(code)
-            sys.stdout = sys.__stdout__
-            sys.stderr = sys.__stderr__
-            if not more: 
-                buffer = []
-                print(output.getvalue)
-            else:
-                pass
-        if not more:
-            print(output.getvalue())
+        try:
+            uinput = input('~> ')
+            if uinput.strip() == 'exit()':
+                print("Exiting interactive console.")
+                break
+            processed = process(uinput)
+            # this works and I have no idea why
+            oldOut = sys.stdout
+            oldErr = sys.stderr
+            newOut = io.StringIO()
+            newErr = io.StringIO()
+            sys.stdout = newOut
+            sys.stderr = newErr
+            
+            try:
+                console.push(processed)
+            finally:
+                # Restore original stdout and stderr
+                sys.stdout = oldOut
+                sys.stderr = oldErr
 
-converted = process(testCase)
-print(converted)
+            output = newOut.getvalue()
+            error = newErr.getvalue()
+
+            if output:
+                print(output, end='')
+            if error:
+                print(error, end='')
+        except (EOFError, KeyboardInterrupt):
+            print("\nExiting interactive console.")
+            break
+        except Exception as e:
+            print(f"Error: {e}")
+else:
+
+    converted = process(testCase)
+    print(converted)
